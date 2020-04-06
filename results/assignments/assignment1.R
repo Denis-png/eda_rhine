ce69d0d2385158479d178181e20cd0d98c9d2ed2
Denis Golovenko <golovenkomail@gmail.com>
2020-04-06 10:21:49 +0200

    Update assignment1.R

--- results/assignments/assignment1.R
+++ results/assignments/assignment1.R
@@ -1,7 +1,7 @@
 TEMPERATURES = c(3, 6, 10, 14)
 WEIGHTS = c(1, 0.8, 1.2, 1)
 library(data.table)
-aa = function(x, y) {
+multiply = function(x, y) {
   x * y
 }
-results <- aa(TEMPERATURES, WEIGHTS)
+results <- multiply(TEMPERATURES, WEIGHTS)
