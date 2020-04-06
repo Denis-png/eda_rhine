5bb03898ab4024b7241094a4283f6de5aaae580e
Denis Golovenko <golovenkomail@gmail.com>
2020-04-06 13:44:28 +0200

    Update 01b_import_runoff_day.R

--- code/01b_import_runoff_day.R
+++ code/01b_import_runoff_day.R
@@ -12,3 +12,13 @@ file_count <- 1
 temp_dt <- fread(paste0(raw_path, fnames[file_count]))
 station_id <- substr(fnames[file_count], 1, id_length)
 temp_dt <- cbind(id = factor(station_id), temp_dt)
+
+temp_dt <- id_sname[temp_dt, on = 'id']
+
+runoff_day_raw <- rbind(runoff_day_raw, temp_dt)
+
+runoff_day_raw[, 'hh:mm' := NULL]
+colnames(runoff_day_raw)[3:4] <- c('date', 'value')
+runoff_day_raw[, date := as.Date(date)]
+
+saveRDS(runoff_day_raw, './data/runoff_day_raw.rds')
