b765c571adee508a0788a29dd94b40f41cb743c5
Denis Golovenko <golovenkomail@gmail.com>
2020-04-07 15:20:56 +0200

    Update 02_preparation.R

--- code/02_preparation.R
+++ code/02_preparation.R
@@ -1,16 +1,21 @@
 library(data.table)
 library(ggplot2)
 
-runoff_stations <- readRDS('./data/runoff_stations.rds')
+runoff_stations <- readRDS('./data/runoff_stations_raw.rds')
 runoff_day <- readRDS('./data/runoff_day_raw.rds')
 
 rees_runoff_day <- runoff_day[sname == 'REES']
+
+ggplot(data = rees_runoff_day) +
+  geom_line(aes(x = date, y = value))
+
+ggplot(data = rees_runoff_day) +
+  geom_point(aes(x = date, y = value))
 
 ggplot(data = rees_runoff_day, 
        aes(x = date, y = value)) +
   geom_point() +
   geom_line()
-
 
 rees_dier_runoff_day <- runoff_day[sname == 'REES' | sname == 'DIER']
 
@@ -57,3 +62,4 @@ runoff_day <- runoff_day[year(date) <= 2016]
 
 saveRDS(runoff_stations, './data/runoff_stations.rds')
 saveRDS(runoff_day, './data/runoff_day.rds')
+
