a1dd5eac7d0dea93f49954c87834659cbed1377b
Denis Golovenko <golovenkomail@gmail.com>
2020-04-08 14:13:30 +0200

    Update assignment3.R

--- results/assignments/assignment3.R
+++ results/assignments/assignment3.R
@@ -31,3 +31,38 @@ runoff_stats_new
 
 #Question 3
 
+runoff_summary <- readRDS('./data/runoff_summary.rds')
+runoff_month <- readRDS('./data/runoff_month.rds')
+
+runoff_month$runoff_class[sname == runoff_summary$sname] <- runoff_summary$runoff_class
+
+ggplot(runoff_month, aes(x = factor(month), y = value, fill= runoff_class)) +
+  facet_wrap(~sname, scales = 'free') +
+  geom_boxplot()
+
+#Question 4
+
+ggplot(runoff_day, aes(x = sname, y = value)) +
+  geom_boxplot() 
+
+#We see that outliers for the stations with the highest altitude are the biggest.
+#This happens because of the different amount of precipitation and snow on different altitudes
+
+#Question 5
+
+runoff_stations <- readRDS('./data/runoff_stations.rds')
+runoff_stations[, area_class := factor('small')]
+runoff_stations[area >= 10000 & area < 100000, area_class := factor('medium')]
+runoff_stations[area >= 100000, area_class := factor('large')]
+
+runoff_stations[, alt_class := factor('low')]
+runoff_stations[altitude >= 10 & altitude < 100, alt_class := factor('medium')]
+runoff_stations[altitude >= 100, alt_class := factor('high')]
+
+runoff_stations$mean_day <- runoff_stats$mean_day
+
+ggplot(runoff_stations, aes(x = mean_day, y = area, col = area_class, cex = alt_class)) +
+  geom_point()
+
+
+
