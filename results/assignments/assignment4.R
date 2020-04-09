2a06778a34d1bbb6552278852907cd7832eb4862
Denis Golovenko <golovenkomail@gmail.com>
2020-04-09 19:06:34 +0200

    Update assignment4.R

--- results/assignments/assignment4.R
+++ results/assignments/assignment4.R
@@ -26,3 +26,25 @@ quantiles <- runoff_day[,.(quantile(value, c(0.1)),quantile(value, c(0.9))), by 
 runoff_day_new <- merge(runoff_day, quantiles, by = 'sname')
 runoff_days_rating <- data.table(above_high = runoff_day_new[value >= V2,.N], below_low = runoff_day_new[value < V1, .N])
 runoff_days_rating
+
+#Question 3
+dt <- runoff_summary[, .(sname, area, category)]
+runoff_stats_short <- runoff_day[year <= 2010, .(sname, mean_day = round(mean(value)), 0), by = sname] # counting new means
+to_plot <- runoff_stats_short[dt, on = 'sname']
+
+ggplot(to_plot, aes(x = mean_day, y = area)) +
+  geom_point(aes(col = category), cex = 3) +
+  geom_smooth(method = 'loess', formula = y ~ x, se = 0, col = colset_4[1]) +
+  scale_color_manual(values = colset_4[c(2, 3, 4)]) +
+  xlab(label = "Area (km3)") +
+  ylab(label = "Runoff (m3/s)") +
+  theme_bw()
+#We can notice that nothing has changed, although mean value in new
+ggplot(to_plot, aes(x = mean_day, y = area)) +
+  geom_point(aes(col = category), cex = 3) +
+  geom_smooth(method = 'lm', formula = y ~ x, se = 0, col = colset_4[1]) +
+  scale_color_manual(values = colset_4[c(2, 3, 4)]) +
+  xlab(label = "Area (km3)") +
+  ylab(label = "Runoff (m3/s)") +
+  theme_bw()
+#Linear model became less accurate
