18c86fd2116ef6e72fb62a119f0a99ee6bc7b6ff
Denis Golovenko <golovenkomail@gmail.com>
2020-04-09 18:43:57 +0200

    Update sourcecode.R

--- code/sourcecode.R
+++ code/sourcecode.R
@@ -12,9 +12,9 @@ runoff_summer <- readRDS('data/runoff_summer.rds')
 runoff_winter <- readRDS('data/runoff_winter.rds')
 runoff_year <- readRDS('data/runoff_year.rds')
 precip_day <- readRDS('data/precip_day.rds')
-
-
-
+runoff_year_key <- readRDS('data/runoff_year_key.rds')
+runoff_month_key <- readRDS('data/runoff_month_key.rds')
+runoff_day <- readRDS('data/runoff_day.rds')
 #Variables
 colset_4 <-  c("#D35C37", "#BF9A77", "#D6C6B9", "#97B8C2")
 theme_set(theme_bw())
